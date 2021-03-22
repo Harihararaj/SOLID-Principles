ba8ae352e80475aee8f17a0d787a17524a52f390
Harihararaj <68691604+Harihararaj@users.noreply.github.com>
2021-03-22 16:16:55 +0530

    Create ImproperCode.java

---
+++ InterfaceSegregationPrinciple/Code/ImproperCode.java
@@ -0,0 +1,30 @@
+package com.company;
+interface Vehicle{
+    public void moving();
+    public void stop();
+    public void reverse();
+}
+class Auto implements Vehicle{
+    public void moving(){
+
+        System.out.println("Auto is moving");
+    }
+    public void stop(){
+
+        System.out.println("Auto is stopped");
+    }
+    public void reverse()//Here we donot need reverse method, Since we used a single interface We are Forced to implement reverse method
+    {                    // Hence violates Interface Segregation Function
+        System.out.println("Auto is reverse");
+    }
+}
+
+public class notFollowingInterfaceSegregationPrinciple {
+
+    public static void main(String[] args) {
+	// write your code here
+        Auto auto=new Auto();
+        auto.moving();
+
+    }
+}
